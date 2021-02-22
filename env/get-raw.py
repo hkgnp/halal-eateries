e3ff46e1c1465f31755b8004a1b43b55e6e0a07f
hkgnp <ben@pngs.cc>
2021-02-23 01:05:08 +0800

    Added alt script to get the raw html

---
+++ env/get-raw.py
@@ -0,0 +1,49 @@
+# import required modules 
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time 
+
+# create instance of Chrome webdriver
+options = Options()
+options.headless = True;
+driver = webdriver.Chrome(options = options) 
+driver.get("https://www.muis.gov.sg/Halal/Halal-Certification/Certified-Eating-Establishments#") 
+
+driver.implicitly_wait(10)
+
+# enter asterisk 
+driver.find_element_by_xpath('//*[@id="txtHalalSearch"]').send_keys('*') 
+
+# click on Search 
+driver.find_element_by_xpath( 
+	'//*[@id="btnHalalSearch"]').click() 
+
+# Total pages: 326
+i=1
+while i < 327:
+    # Get page content
+    print('Pushing Page: ' + str(i))
+
+    # Get results div
+    results = driver.find_element_by_xpath('//*[@id="results"]/div')
+
+    # Use BS to parse results
+    soup = BeautifulSoup(results.get_attribute('innerHTML'), 'html.parser')
+    driver.implicitly_wait(10)
+
+    # Use BS to get only p tags
+    output = soup.find_all("p")
+
+    # Output to html file
+    with open("halal-eateries.html", "a") as file:
+        file.write(str(output))
+
+    # Prepare to click to next page
+    page = f"//*[@id='{i+1}']"
+    driver.find_element_by_xpath(page).click()
+
+    # Important as next page results take time to load
+    time.sleep(8)
+    i+=1
